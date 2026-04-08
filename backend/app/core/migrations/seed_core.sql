d535593904b4bbba2ecb9a253509f7d525f344c2
GARO21225 <edgar.kouame@orange.com>
2026-04-08 19:16:00 +0000

    Add files via upload

--- backend/app/core/migrations/seed_core.sql
+++ backend/app/core/migrations/seed_core.sql
@@ -2,22 +2,22 @@ INSERT INTO utilisateur
     (email, mot_de_passe_hash, nom, prenom, role, actif, email_verifie)
 VALUES (
     'admin@sig-ftth.ci',
-    '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBpj2sBGmFBuG2',
+    '$2b$12$pCQT.vGzl9vg/KvUj5zxEO5dOjQTqfIPN5RTcA5HXxrzKiX4SFZLC',
     'KOUAME','Edgar','admin', TRUE, TRUE
-) ON CONFLICT (email) DO NOTHING;
+) ON CONFLICT (email) DO UPDATE SET mot_de_passe_hash = EXCLUDED.mot_de_passe_hash;
 
 INSERT INTO utilisateur
     (email, mot_de_passe_hash, nom, prenom, role, actif, email_verifie)
 VALUES (
     'commercial@sig-ftth.ci',
-    '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBpj2sBGmFBuG2',
+    '$2b$12$pCQT.vGzl9vg/KvUj5zxEO5dOjQTqfIPN5RTcA5HXxrzKiX4SFZLC',
     'DIALLO','Aminata','commercial', TRUE, TRUE
-) ON CONFLICT (email) DO NOTHING;
+) ON CONFLICT (email) DO UPDATE SET mot_de_passe_hash = EXCLUDED.mot_de_passe_hash;
 
 INSERT INTO utilisateur
     (email, mot_de_passe_hash, nom, prenom, role, actif, email_verifie)
 VALUES (
     'technicien@sig-ftth.ci',
-    '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBpj2sBGmFBuG2',
+    '$2b$12$pCQT.vGzl9vg/KvUj5zxEO5dOjQTqfIPN5RTcA5HXxrzKiX4SFZLC',
     'BAMBA','Seydou','technicien', TRUE, TRUE
-) ON CONFLICT (email) DO NOTHING;
+) ON CONFLICT (email) DO UPDATE SET mot_de_passe_hash = EXCLUDED.mot_de_passe_hash;
